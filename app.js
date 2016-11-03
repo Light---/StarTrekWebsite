74cecbb6bb8f3707655f8e607d6a59f9e6809725
Light--- <eleary01@qub.ac.uk>
2016-11-03 15:40:06 +0000

    Updates to Node

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ var http = require('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Server test one two three!\n");
+  	response.writeHead(200);
+var express = require('express');
+var app = express();
+app.use(express.static('public'))
+app.use('/static', express.static('public'))
+
+ app.get('/Comments', function (req, res) {
+  res.send('CommentsPage.html');
+ });
+
+  app.get('/MainPage', function (req, res) {
+  res.send('MainPage.html');
+  });
+
+  app.get('/', function (req, res) {
+  res.send('MainPage.html');
+  });
+
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000, "10.10.10.39");
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+server.listen(8000);
+console.log("Server running at LocalHost");
