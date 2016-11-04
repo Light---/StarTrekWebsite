a1285cf7167aaea43b471579c0f8534e1a12ce84
Light--- <eleary01@qub.ac.uk>
2016-11-04 17:19:38 +0000

    node.js and static files working with localhost

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
-// Load the http module to create an http server.
-var http = require('http');
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  	response.writeHead(200);
 var express = require('express');
 var app = express();
+
 app.use(express.static('public'))
-app.use('/static', express.static('public'))
 
- app.get('/Comments', function (req, res) {
-  res.send('CommentsPage.html');
- });
-
-  app.get('/MainPage', function (req, res) {
-  res.send('MainPage.html');
-  });
-
-  app.get('/', function (req, res) {
-  res.send('MainPage.html');
-  });
-
-});
-
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+// Listen on Port Number and IP Address
+app.listen(3000, function () {
 console.log("Server running at LocalHost");
+})
