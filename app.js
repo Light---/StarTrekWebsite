3bbde136a528af3dc9f5b7bb2706916107903682
Light--- <eleary01@qub.ac.uk>
2016-11-05 19:00:59 +0000

    serverside comments to be called

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'))
+var comment1 = {
+	id: 1,
+	title: "Test",
+	message: "This is a test comment"
+};
+var comment2 = {
+	id: 2,
+	title: "Teacher Alab",
+	message: "Tech tapa are funz"
+};
+var comments = [
+	comment1,
+	comment2
+];
+
+app.use(express.static('public'));
+
+app.get('/comments', function (req, res) {
+  res.json(comments);
+});
+
 
 // Listen on Port Number and IP Address
 app.listen(3000, function () {
-console.log("Server running at LocalHost");
-})
+	console.log("Server running at LocalHost");
+});
